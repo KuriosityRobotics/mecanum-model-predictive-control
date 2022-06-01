04cba069154641e91cdd7f9cd5840fd037bc4903
chop@quenda.net <chop@quenda.net>
2022-06-01 00:21:23 -0700

    removed

--- src/model-predictive-control/qpqc.m
+++
@@ -1,8 +0,0 @@
-addpath 'DynamicSolver'
-addpath '../../Downloads/forces_pro_client'
-addpath '../../Downloads/casadi-windows-matlabR2016a-v3.5.5'
-disp(ForcesFindDumpedProblems())
-[model, codeoptions, outputs, additionalData, problems] =  ForcesLoadSymbolicDump( 'DynamicSolver_SC015F9SE8O_20225301456404177_A.json' );
-
-
-stages2qcqp(@DynamicSolver, problems, problems(1))
